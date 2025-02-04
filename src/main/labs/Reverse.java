8ec08e518fc1d420d858462b83763a3d43a90a9b
Ishaan Ghaskadbi <ishaanghaskadbi@Ishaans-MacBook-Air.local>
2025-02-04 08:22:29 -0500

    reverse array lab with comments

--- src/main/labs/Reverse.java
+++ src/main/labs/Reverse.java
@@ -7,32 +7,34 @@ public class Reverse {
 
         System.out.println("Please input a positive integer: ");
         Scanner obj = new Scanner(System.in);
-        int x = obj.nextInt();
+        int x = obj.nextInt(); // finds the length of the array
 
         System.out.println("Enter " + x + " values: ");
-        int [] nums = new int[x];
+        int [] nums = new int[x]; // makes an array which has the length
 
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = obj.nextInt();
+            nums[i] = obj.nextInt(); // gives elements of array to the user
         }
         for (int i : (nums)) {
-            System.out.print(i + " ");
+            System.out.print(i + " "); // to print out the original array
         }
         System.out.println();
         for (int i : reverseArray(nums)) {
-            System.out.print(i + " ");
+            System.out.print(i + " "); // to print out the reverse array
         }
 
     }
 
     public static int [] reverseArray (int [] arr) {
+
         int temp = 0;
 
-        for (int i = 0; i < arr.length / 2; i++) {
+        for(int i = 0; i < arr.length / 2; i++) {
             temp = arr[i];
-            arr [i] = arr [arr.length - 1 - i];
-            arr [arr.length - 1 - i] = temp;
+            arr[i] = arr[arr.length - 1 - i]; // To assign values to the last element of array
+            arr[arr.length - 1 - i] = temp; // To assign values to the first element of array
         }
         return arr;
+
     }
 }
